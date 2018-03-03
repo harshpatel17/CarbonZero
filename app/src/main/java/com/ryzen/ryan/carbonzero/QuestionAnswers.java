b24c495438fc74d5c47ed520ad15b9c5cc9d4886
Ryan Zeng <zeng.ryan2@gmail.com>
2018-03-02 23:59:08 -0500

    Added QuestionsPage

---
+++ app/src/main/java/com/ryzen/ryan/carbonzero/QuestionAnswers.java
@@ -0,0 +1,12 @@
+package com.ryzen.ryan.carbonzero;
+
+/**
+ * Created by Ryan on 3/2/2018.
+ */
+
+public class QuestionAnswers {
+    Boolean completed = false;
+    public QuestionAnswers(Boolean completed){
+        this.completed = true;
+    }
+}
--- app/src/main/java/com/ryzen/ryan/carbonzero/QuestionsActivity.java
+++ app/src/main/java/com/ryzen/ryan/carbonzero/QuestionsActivity.java
@@ -1,5 +1,6 @@
 package com.ryzen.ryan.carbonzero;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,10 @@ public class QuestionsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_questions);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+
     }
 }

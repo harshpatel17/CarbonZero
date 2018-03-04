752ea8fc2eef976195f3b663b2d63df4299c6fed
Harsh Patel <harshpatel7.list@gmail.com>
2018-03-04 03:42:32 -0500

    Added login page

--- app/src/main/java/com/ryzen/ryan/carbonzero/QuestionSeven.java
+++ app/src/main/java/com/ryzen/ryan/carbonzero/QuestionSeven.java
@@ -1,7 +1,9 @@
 package com.ryzen.ryan.carbonzero;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class QuestionSeven extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class QuestionSeven extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question_seven);
+
+        // Hides action bar
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
+        // Hides status bar
+        View decorView = getWindow().getDecorView();
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
     }
 }

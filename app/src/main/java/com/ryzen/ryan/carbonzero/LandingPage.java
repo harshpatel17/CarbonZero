be0fca0aa7d7783fd2f2fc67d1b792bbf6d3328f
mchang16 <mchang0816@gmail.com>
2018-03-03 23:28:43 -0500

    Merge branch 'BackEndDev' of https://github.com/zryan2/CarbonZero into Algorithm
    # Conflicts:
    #	app/build.gradle
    #	app/src/main/java/com/ryzen/ryan/carbonzero/LandingPage.java

--- app/src/main/java/com/ryzen/ryan/carbonzero/LandingPage.java
+++ app/src/main/java/com/ryzen/ryan/carbonzero/LandingPage.java
@@ -3,14 +3,13 @@ package com.ryzen.ryan.carbonzero;
 import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
+import android.os.Bundle;
 import android.os.Environment;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -21,7 +20,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.nio.Buffer;
 
 public class LandingPage extends AppCompatActivity {
 
@@ -52,19 +50,19 @@ public class LandingPage extends AppCompatActivity {
         landingLayout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                file = new File(path+"/prelim_data.txt");
-                if(file.isFile()) {
-                    initialFile(file);
-                }
-                String [] data = Load(file);
-                if (data[0].equals("false")){
-                    Toast.makeText(getApplicationContext(), "It is false!", Toast.LENGTH_SHORT).show();
-                    Intent questionIntent = new Intent(LandingPage.this, QuestionsActivity.class);
-                    startActivity(questionIntent);
-                }else{
-                    Intent earthIntent = new Intent(LandingPage.this, EarthActivity.class);
-                    startActivity(earthIntent);
-                }
+//                file = new File(path+"/prelim_data.txt");
+//                if(file.isFile()) {
+//                    initialFile(file);
+//                }
+//                String [] data = Load(file);
+//                if (data[0].equals("false")){
+//                    Toast.makeText(getApplicationContext(), "It is false!", Toast.LENGTH_SHORT).show();
+//                    Intent questionIntent = new Intent(LandingPage.this, QuestionsActivity.class);
+//                    startActivity(questionIntent);
+//                }else{
+//                    Intent earthIntent = new Intent(LandingPage.this, EarthActivity.class);
+//                    startActivity(earthIntent);
+//                }
 //                try{
 //                    Intent questionIntent = new Intent(LandingPage.this, QuestionsActivity.class);
 //                    startActivity(questionIntent);
